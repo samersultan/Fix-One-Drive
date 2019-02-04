7d32ff7704e7243974d75f51b7545db4d5dbee0f
samersultan1 <samersultan1@gmail.com>
2019-02-04 16:45:03 -0600

    Update Fix-One-Drive.bat

--- Fix-One-Drive.bat
+++ Fix-One-Drive.bat
@@ -1,4 +1,17 @@
-@ECHO OFF
+@echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate.
+    )
+
+    pause >nul
 reg ADD "HKLM\Software\Policies\Microsoft\Windows\OneDrive" /v DisableFileSyncNGSC /t REG_DWORD /D 0 /F
 taskkill /f /im explorer.exe
 start explorer.exe
